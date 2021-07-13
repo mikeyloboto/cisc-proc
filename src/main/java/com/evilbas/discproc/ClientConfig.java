0645a20884841514eb3dd259c96c7c809c681ec2
Evilbas <dimonloboto@gmail.com>
2021-07-13 19:07:23 -0400

    metrics

--- src/main/java/com/evilbas/discproc/ClientConfig.java
+++ src/main/java/com/evilbas/discproc/ClientConfig.java
@@ -6,14 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@PropertySources({ @PropertySource("classpath:application.properties"),
-		@PropertySource("file:${APPLICATION_EXT_PROPERTIES}/discproc-ws/env.properties") })
+@PropertySource("classpath:application.properties")
+@PropertySource("file:${APPLICATION_EXT_PROPERTIES}/discproc-ws/env.properties")
 @MapperScan("com.evilbas.discproc.dao.sql")
 @ComponentScan(basePackages = "com.evilbas.discproc")
+@ComponentScan("com.evilbas.metrics")
 @EnableAsync
 public class ClientConfig {
 
